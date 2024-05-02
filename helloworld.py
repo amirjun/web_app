34e595031c86bc2a4d3c62f476bb87fb8d531f96
amirjun <140188225+amirjun@users.noreply.github.com>
2024-05-02 18:08:06 +0300

    Add files via upload

--- helloworld.py
+++ helloworld.py
@@ -1,16 +1,19 @@
 
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__, template_folder = '', static_folder='')   
 def index():
-    
     return render_template('index.html') 
 
 def second():
     return render_template('second.html')
-app = Flask(__name__, template_folder = '', static_folder='')   
+
+@app.route('/registr', methods=('GET', 'POST'))
+def registr():
+    return render_template('registr.html')
 
 app.add_url_rule('/', 'index', index)  
 app.add_url_rule('/second', 'second', second)
 if __name__ == "__main__":
     
-    app.run(debug=True) 
+    app.run(debug=True)
